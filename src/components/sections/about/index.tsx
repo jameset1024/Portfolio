792c16e2f64c51b92e61fd8564d975b565e9d098
Erik Thomas <erik@brightthought.co>
2023-11-20 14:23:51 -0500

    Update resume link

--- src/components/sections/about/index.tsx
+++ src/components/sections/about/index.tsx
@@ -1,37 +1,60 @@
 import * as React from "react";
 import "./styles.sass";
-import {ButtonExternal} from "@app/components/elements/button";
+import { ButtonExternal } from "@app/components/elements/button";
 
 const AboutPage = () => {
   return (
-    <section className={'about-section'} id={'about'}>
-      <div className={'section-header-wrap'}>
-        <div className={'section-header-text'}>
-          About
-        </div>
+    <section className={"about-section"} id={"about"}>
+      <div className={"section-header-wrap"}>
+        <div className={"section-header-text"}>About</div>
       </div>
 
-      <div className={'about-info'}>
-        <div className={'about-inner'}>
+      <div className={"about-info"}>
+        <div className={"about-inner"}>
           <div>
             <h2>Charlotte Resident, Pittsburgh Made</h2>
-            <p>Hello! My name is Erik and I'm a full-stack software engineer currently residing in Charlotte, NC. I've been doing this internet thing since around 2013 and I've been passionate about it ever since. It's been a long and interesting journey to this point, learning and experiencing new and interesting things to help me grow as a developer.</p>
-            <p>I love learning new things, trying different programming languages, techniques, libraries, etc... It's just a never ending journey of information gathering and I think that's what I love most about this industry.</p>
-            <p>When I'm not working on projects either for work or myself, I unwind with some time at the gym, anime, video games, and basketball.</p>
+            <p>
+              Hello! My name is Erik and I'm a full-stack software engineer
+              currently residing in Charlotte, NC. I've been doing this internet
+              thing since around 2013 and I've been passionate about it ever
+              since. It's been a long and interesting journey to this point,
+              learning and experiencing new and interesting things to help me
+              grow as a developer.
+            </p>
+            <p>
+              I love learning new things, trying different programming
+              languages, techniques, libraries, etc... It's just a never ending
+              journey of information gathering and I think that's what I love
+              most about this industry.
+            </p>
+            <p>
+              When I'm not working on projects either for work or myself, I
+              unwind with some time at the gym, anime, video games, and
+              basketball.
+            </p>
 
-            <ButtonExternal to={'https://erik-portfolio.s3.amazonaws.com/ET_Resume.pdf'} width={'250px'} target={'_black'}>Download My Resume</ButtonExternal>
+            <ButtonExternal
+              to={
+                "https://erik-portfolio.s3.amazonaws.com/ErikThomasResume.pdf"
+              }
+              width={"250px"}
+              target={"_black"}
+            >
+              Download My Resume
+            </ButtonExternal>
           </div>
           <div>
-            <div className={'headshot-wrapper'}>
+            <div className={"headshot-wrapper"}>
               <img
-                src={'https://erik-portfolio.s3.amazonaws.com/B11A9094.jpg'}
-                alt={'Erik Thomas'} />
+                src={"https://erik-portfolio.s3.amazonaws.com/B11A9094.jpg"}
+                alt={"Erik Thomas"}
+              />
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
